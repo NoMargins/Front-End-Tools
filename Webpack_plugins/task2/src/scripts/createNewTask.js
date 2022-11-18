f8325f996c81bc14bd42f21ef35e10d303277120
Olena Ryabenko <olenaryabenko@gmail.com>
2022-11-18 19:45:45 +0200

    initial commit

---
+++ Webpack_plugins/task2/src/scripts/createNewTask.js
@@ -0,0 +1,37 @@
+import { renderTasks } from './renderTasks.js';
+import { setItem } from './storage.js';
+import { createTask, getTasksList } from './tasksGateway.js';
+import './actions.scss';
+
+const newTaskElem = document.querySelector('.task-input');
+const submitElem = document.querySelector('.create-task-btn');
+
+const newTaskFunction = () => {
+	const newTask = {
+		text: `${newTaskElem.value}`,
+		done: false,
+		createDate: new Date().toISOString(),
+	};
+
+	createTask(newTask)
+		.then(() => getTasksList())
+		.then((newTasksList) => {
+			setItem('tasksList', newTasksList);
+			renderTasks();
+		});
+};
+
+const submitFunction = () => {
+	if (!newTaskElem.value) {
+		newTaskElem.classList.add('task-input__notice');
+		return;
+	} else {
+		newTaskElem.classList.remove('task-input__notice');
+		newTaskFunction();
+		renderTasks();
+		newTaskElem.value = '';
+	}
+};
+
+export const submitNewElemFunction = () =>
+	submitElem.addEventListener('click', submitFunction);
